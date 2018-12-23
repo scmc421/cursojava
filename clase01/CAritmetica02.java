84f15f5c0e1ac68c28ca5c35b8e09782e4af043b
scmc421 <scmc.421@gmail.com>
2018-12-22 21:29:46 -0500

    Operadores básicos
    Co-Authored-By: Morgan Mc <scmc421@users.noreply.github.com>

--- clase01/CAritmetica02.java
+++ clase01/CAritmetica02.java
@@ -4,7 +4,7 @@ class CAritmetica02
     double a = 0;
     float b = 2/3f;
     int c = 0, r = 0;
-    char car = 97; // corresponde al ASCII de la abstract
+    char car = 97; // corresponde al ASCII de la a
     System.out.println("Valor de a = " + a + " valor de b = " + b + " valor de c = " + c + " valor de r = " + r + " valor de car = " + car);
   }
 }
