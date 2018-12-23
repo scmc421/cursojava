477af5b598f0ca118606f1adb8d31fe76c24dc2e
scmc421 <scmc.421@gmail.com>
2018-12-23 18:44:05 -0500

    Asignar datos

---
+++ clase02/CAsignarDatos.java
@@ -0,0 +1,13 @@
+{
+  public static void main(String[] args) {
+    double a = 0, b = 0;
+    int c = 0;
+    String s;
+    a = 3.14; b = 2.71; c = 2;
+    s =  "Datos";
+    System.out.println(s + ": ")
+    System.out.println(" a = " + a);
+    System.out.println(" b = " + b);
+    System.out.println(" c = " + c);
+  }
+}
