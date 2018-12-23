aab3bbaab83eef140c5221a04f9854af2fbd8ee7
scmc421 <scmc.421@gmail.com>
2018-12-22 21:27:49 -0500

    Aritmetica básica
    Co-Authored-By: Morgan Mc <scmc421@users.noreply.github.com>

---
+++ clase01/CAritmetica02.java
@@ -0,0 +1,10 @@
+class CAritmetica02
+{ public static void main(String[] args)
+  {
+    double a = 0;
+    float b = 2/3f;
+    int c = 0, r = 0;
+    char car = 97; // corresponde al ASCII de la abstract
+    System.out.println("Valor de a = " + a + " valor de b = " + b + " valor de c = " + c + " valor de r = " + r + " valor de car = " + car);
+  }
+}
