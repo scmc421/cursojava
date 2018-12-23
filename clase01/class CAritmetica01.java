8c3fa7b64b4fc9bdce59c65a9324777272584182
scmc421 <scmc.421@gmail.com>
2018-12-22 19:29:24 -0500

    Aritmetica basica
    Co-Authored-By: Morgan Mc <scmc421@users.noreply.github.com>

---
+++ clase01/class CAritmetica01.java
@@ -0,0 +1,20 @@
+class CAritmetica01
+{
+  /*operaciones aritméticas*/
+  public static void main (String[] args)
+  int dato1, dato2, dato3, resultado;
+  dato1 = 20;
+  dato2 = 10;
+  //suma
+  resultado = dato1 + dato2;
+  System.out.println(dato1+" + "+dato2+" = "+resultado)
+  //resta
+  resultado = dato1 - dato2;
+  System.out.println(dato1+" + "+dato2+" = "+resultado)
+  //multiplicacion
+  resultado = dato1 * dato2;
+  System.out.println(dato1+" + "+dato2+" = "+resultado)
+  //división
+  resultado = dato1 / dato2;
+  System.out.println(dato1+" + "+dato2+" = "+resultado)
+}
