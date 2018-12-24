f1af3ecc5fbb7ad0b5c59b814f80f0edb5455b7c
scmc421 <scmc.421@gmail.com>
2018-12-23 22:34:18 -0500

    If else par e impar

--- clase03/CRelacion02.java
+++ clase03/CRelacion02.java
@@ -1,5 +1,12 @@
 class CRelacion02{
   public static void main(String[] args) {
-    
+    int num = 23;
+    if ( num % 2 == 0){
+      System.out.println("El valor: " + num + "Es par");
+    }
+    else
+    {
+      System.out.println("El valor: " + num + "Es impar");
+    }
   }
 }
