64aeef6bc735f32b699e16e037ddc4eecc5bfd3f
scmc421 <scmc.421@gmail.com>
2018-12-23 22:10:25 -0500

    Expresiones condicionales

--- clase03/CRelacion01.java
+++ clase03/CRelacion01.java
@@ -1,8 +1,14 @@
 class CRelacion01{
   public static void main(String[] args) {
     int num = 24;
-    if (num % 2 == 0) // si el residuo de la división es cero
-    System.out.println("Número par");
-    System.out.println("Valor: " + num);
+    if (num % 2 == 0) // Par si el residuo de la división es cero
+      {
+        System.out.println("Número par, Valor: " + num);
+      }
+    int num2 = 23;
+    if (num2 % 2 != 0) //Impar si el residuo de la división es cero
+      {
+        System.out.println("Número IMpar, Valor: " + num2);
+      }
   }
 }
