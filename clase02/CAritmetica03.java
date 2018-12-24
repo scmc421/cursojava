4158b640494a9143b5fdd39335e90842b3610d06
scmc421 <scmc.421@gmail.com>
2018-12-23 19:27:57 -0500

    Conversión explícita a tipo int

---
+++ clase02/CAritmetica03.java
@@ -0,0 +1,11 @@
+/*operaciones aritméticas*/
+class CAritmetica03
+{
+  public static void main(String[] args) {
+    double a = 10;
+    float b = 20;
+    int c = 2, r = 0;
+    r = (int)(7.5 * Math.sqrt(a) - b / c);
+    System.out.println(r);
+  }
+}
